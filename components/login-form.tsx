f66a1f385b8b3b758691642063a7ae75a70e9f6d
Jonathan Carnos <jonathan.carnos@gmail.com>
2022-03-15 20:56:03 +0200

    Oauth

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useForm } from 'react-hook-form'
 import { Button } from '@mui/material'
 import { useRouter } from 'next/router'
 import Link from 'next/link'
 import { signIn } from 'next-auth/react'
+import { MdFacebook } from 'react-icons/md'
+import { AiFillGoogleCircle } from 'react-icons/ai'
 
 import Field from './field'
 import { setUsername } from '../redux/slice'
@@ -19,13 +21,18 @@ const styles = {
   width: '100%',
 }
 
+const nodeEl: any = {
+  facebook: MdFacebook,
+  google: AiFillGoogleCircle,
+}
+
 export default function LoginForm({ dispatch, providers }: LoginFormProps) {
   const {
     control,
     handleSubmit,
     watch,
     formState: { errors },
-  } = useForm({ defaultValues: { username: '', password: '' } })
+  } = useForm({ defaultValues: { email: '' } })
 
   const router = useRouter()
 
@@ -35,6 +42,10 @@ export default function LoginForm({ dispatch, providers }: LoginFormProps) {
     router.push('/')
     console.log(data)
   }
+
+  const oauthIcon = useCallback((id) => {
+    return id !== 'email' ? React.createElement(nodeEl[id], null) : <></>
+  }, [])
   return (
     <form
       className="shadow-xl hover:shadow-2xl flex flex-col py-8 px-14 bg-slate-50"
@@ -46,34 +57,48 @@ export default function LoginForm({ dispatch, providers }: LoginFormProps) {
       >
         Connect yourself
       </h1>
-      <Field control={control} name="username" sx={styles} />
-      {/* <Field control={control} name="password" sx={styles} /> */}
-      <div style={{ display: 'flex', justifyContent: 'end' }}>
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{
-            color: 'black',
-            '&:hover': {
-              color: 'white',
-            },
-          }}
-        >
-          Submit
-        </Button>
-      </div>
-      <div className="text-blue-400 hover:text-blue-500">
-        <Link href="/signup">Sign up</Link>
-      </div>
+      {/*  <Field control={control} name="username" sx={styles} />
+    //    <Field control={control} name="password" sx={styles} />
+    //   <div style={{ display: 'flex', justifyContent: 'end' }}>
+    //     <Button
+    //       type="submit"
+    //       variant="contained"
+    //       sx={{
+    //         color: 'black',
+    //         '&:hover': {
+    //           color: 'white',
+    //         },
+    //       }}
+    //     >
+    //       Submit
+    //     </Button>
+    //   </div>
+    //   <div className="text-blue-400 hover:text-blue-500">
+    //     <Link href="/signup">Sign up</Link>
+  </div> */}
+      <>
+        {Object.values(providers).map((provider: any) => {
+          console.log(provider)
+          return (
+            <div key={provider.name} className="mt-4">
+              {provider.id === 'email' && (
+                <div>
+                  <hr className="my-4" />
+                  <Field control={control} name="email" sx={styles} />
+                </div>
+              )}
+              <Button
+                variant="contained"
+                onClick={() => signIn(provider.id)}
+                className="bg-slate-200 text-black hover:text-white"
+                startIcon={oauthIcon(provider.id)}
+              >
+                Sign in with {provider.name}
+              </Button>
+            </div>
+          )
+        })}
+      </>
     </form>
-    // <>
-    //   {Object.values(providers).map((provider: any) => (
-    //     <div key={provider.name}>
-    //       <button onClick={() => signIn(provider.id)}>
-    //         Sign in with {provider.name}
-    //       </button>
-    //     </div>
-    //   ))}
-    // </>
   )
 }
